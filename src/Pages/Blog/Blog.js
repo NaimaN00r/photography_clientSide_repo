b53dbe4a704300f4f5278641fb018e0e754f8095
NaimaN00r <anaima407@gmail.com>
2022-11-09 03:27:22 +0600

    4th commit

--- src/Pages/Blog/Blog.js
+++ src/Pages/Blog/Blog.js
@@ -40,6 +40,17 @@ As a result, it is used to create network-centric applications. It's a networked
 </p>
             
             </div>
+            <div className='border-2 border-black bg-red-50 p-3 m-5'>
+            <h1 className='text-center'>How does NodeJS handle multiple requests at the same time?</h1>
+            <p>We know NodeJS application is single-threaded. Say, if processing involves request A that takes 10 seconds, it does not mean that a request which comes after this request needs to wait 10 seconds to start processing because NodeJS event loops are only single-threaded. The entire NodeJS architecture is not single-threaded. 
+
+How NodeJS handle multiple client requests?
+
+NodeJS receives multiple client requests and places them into EventQueue. NodeJS is built with the concept of event-driven architecture. NodeJS has its own EventLoop which is an infinite loop that receives requests and processes them. EventLoop is the listener for the EventQueue. 
+
+If NodeJS can process the request without I/O blocking then the event loop would itself process the request and sends the response back to the client by itself. But, it is possible to process multiple requests parallelly using the NodeJS cluster module or worker_threads module.</p>
+            
+            </div>
        
         </div>
         
